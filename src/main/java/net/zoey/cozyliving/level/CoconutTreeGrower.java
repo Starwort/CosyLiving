8b729331ae16ab09fc7dfa6a0d76e171bc944973
Zoey <Azophyte@gmail.com>
2026-01-01 19:54:55 +0000

    is this even needed like

--- src/main/java/net/zoey/cozyliving/level/CoconutTreeGrower.java
+++ src/main/java/net/zoey/cozyliving/level/CoconutTreeGrower.java
@@ -4,6 +4,7 @@ import net.minecraft.resources.*;
 import net.minecraft.util.*;
 import net.minecraft.world.level.block.grower.*;
 import net.minecraft.world.level.levelgen.feature.*;
+import net.zoey.cozyliving.level.gen.ModConfiguredFeatures;
 import org.jetbrains.annotations.*;
 
 public class CoconutTreeGrower extends AbstractTreeGrower {
@@ -12,7 +13,6 @@ public class CoconutTreeGrower extends AbstractTreeGrower {
         @NotNull RandomSource random,
         boolean hasFlowers
     ) {
-        // return ModConfiguredFeatures.COCONUT_TREE;
-        return null;
+        return ModConfiguredFeatures.COCONUT_TREE_KEY;
     }
 }
