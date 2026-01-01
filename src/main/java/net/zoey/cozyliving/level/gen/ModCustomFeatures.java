39ff399c96eb27ea8ce48923215091f6c26e073f
Zoey <Azophyte@gmail.com>
2026-01-01 19:57:21 +0000

    Made this class so the registry would only be used once instead of twice

---
+++ src/main/java/net/zoey/cozyliving/level/gen/ModCustomFeatures.java
@@ -0,0 +1,19 @@
+package net.zoey.cozyliving.level.gen;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.zoey.cozyliving.CozyLiving;
+import net.zoey.cozyliving.level.RaspberryBushesFeature;
+import net.zoey.cozyliving.level.gen.coconut_tree.CoconutTreeFeature;
+
+public class ModCustomFeatures {
+    public static void RegisterFeatures(IEventBus modEventBus){
+
+        var registry = CozyLiving.FEATURES;
+
+        CozyLiving.LOGGER.info("Registering custom features");
+        CoconutTreeFeature.register();
+        RaspberryBushesFeature.register();
+
+        registry.register(modEventBus);
+    }
+}
