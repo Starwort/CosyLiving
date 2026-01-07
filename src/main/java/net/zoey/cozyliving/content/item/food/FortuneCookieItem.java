5590f5bc15ce0fee0b348a07e067b9439a2fd770
starwort <tcphone93@gmail.com>
2026-01-07 16:45:59 +0000

    Add new Elo fortunes, fix bug with random numbers not being able to pick 99

--- src/main/java/net/zoey/cozyliving/content/item/food/FortuneCookieItem.java
+++ src/main/java/net/zoey/cozyliving/content/item/food/FortuneCookieItem.java
@@ -14,8 +14,9 @@ import net.zoey.cozyliving.content.common.*;
 import net.zoey.cozyliving.content.item.TooltipItem;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.*;
+import java.time.format.*;
+import java.util.*;
 
 public class FortuneCookieItem extends TooltipItem {
     public FortuneCookieItem() {
@@ -45,6 +46,8 @@ public class FortuneCookieItem extends TooltipItem {
         Fortunes.add("fortune.cozyliving.elo.12");
         Fortunes.add("fortune.cozyliving.elo.13");
         Fortunes.add("fortune.cozyliving.elo.14");
+        Fortunes.add("fortune.cozyliving.elo.15");
+        Fortunes.add("fortune.cozyliving.elo.16");
         Fortunes.add("fortune.cozyliving.chan.1");
         Fortunes.add("fortune.cozyliving.zoey.1");
         Fortunes.add("fortune.cozyliving.zoey.2");
@@ -91,6 +94,9 @@ public class FortuneCookieItem extends TooltipItem {
         return stack;
     }
 
+    static final String[] NS = new String[] {"N", "S"};
+    static final String[] EW = new String[] {"E", "W"};
+
     private static @NotNull ItemStack generateFortune(
         @NotNull Level level,
         Player player
@@ -99,17 +105,30 @@ public class FortuneCookieItem extends TooltipItem {
         var rand = level.getRandom();
         var mods = ModList.get().getMods();
         var randomModName = mods.get(rand.nextInt(mods.size())).getDisplayName();
+        var daysInFuture = rand.nextInt(365, 3653);
+        var futureDate = LocalDate
+            .now()
+            .plusDays(daysInFuture)
+            .format(DateTimeFormatter.ofPattern("ccc, d MMM yyyy"));
+
         // Format parameters:
-        // XX XX XX XX [four numbers 0-99] $random_mod_name $obfuscated_random_mod_name $player_name
+        // XX XX XX XX [four numbers 0-99] $random_mod_name $obfuscated_random_mod_name
+        // $player_name $random_future_date NN WW [random coordinates; 0-89 for N, 0-189 for W]
+        // $random_NS $random_EW
         FortunePaper.setHoverName(Component.translatable(
             Fortunes.get(rand.nextInt(Fortunes.size() - 1)),
-            rand.nextInt(99),
-            rand.nextInt(99),
-            rand.nextInt(99),
-            rand.nextInt(99),
+            rand.nextInt(100),
+            rand.nextInt(100),
+            rand.nextInt(100),
+            rand.nextInt(100),
             randomModName,
             "§k" + randomModName,
-            player.getDisplayName()
+            player.getDisplayName(),
+            futureDate,
+            rand.nextInt(90),
+            rand.nextInt(180),
+            NS[rand.nextInt(2)],
+            EW[rand.nextInt(2)]
         ));
         return FortunePaper;
     }
