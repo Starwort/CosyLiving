639b0c5b6275236d471c4421a2e97d3508fc2c5c
Zoey <Azophyte@gmail.com>
2026-01-14 10:39:52 +0000

    Necessary for modifying vanilla loot tables

---
+++ src/main/java/net/zoey/cozyliving/util/ModLootTableModifiers.java
@@ -0,0 +1,27 @@
+package net.zoey.cozyliving.util;
+
+import com.mojang.serialization.Codec;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
+import net.minecraftforge.common.loot.LootModifier;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.zoey.cozyliving.CozyLiving;
+import org.jetbrains.annotations.NotNull;
+
+public class ModLootTableModifiers{
+    public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> LOOT_MODIFIER_SERIALIZERS =
+            DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, CozyLiving.MODID);
+
+    public static final RegistryObject<Codec<? extends IGlobalLootModifier>> ADD_ITEM =
+            LOOT_MODIFIER_SERIALIZERS.register("add_item", AddItemModifier.CODEC);
+
+    public static void register(IEventBus eventBus) {
+        LOOT_MODIFIER_SERIALIZERS.register(eventBus);
+    }
+}
