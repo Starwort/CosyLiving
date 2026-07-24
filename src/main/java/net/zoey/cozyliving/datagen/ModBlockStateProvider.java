543693ab7704edb8c63b0e0e8a680354eb4970cb
Zoey <Azophyte@gmail.com>
2026-07-25 00:33:21 +0100

    Trellised coconut sapling points towards the correct (custom) model file

--- src/main/java/net/zoey/cozyliving/datagen/ModBlockStateProvider.java
+++ src/main/java/net/zoey/cozyliving/datagen/ModBlockStateProvider.java
@@ -132,7 +132,7 @@ public class ModBlockStateProvider extends BlockStateProvider {
             )
         );
         saplingBlock(ModBlocks.COCONUT_SAPLING);
-        saplingBlock(ModBlocks.TRELLISED_COCONUT_SAPLING);
+        simpleBlock(ModBlocks.TRELLISED_COCONUT_SAPLING.block(), modelFile(modLoc("block/trellised_coconut_sapling")));
 
         pressurePlateBlock(
             (PressurePlateBlock) ModBlocks.COCONUT_PRESSURE_PLATE.block(),
