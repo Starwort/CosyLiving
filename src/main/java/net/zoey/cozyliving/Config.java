c9def608e275bb7fab0844a3e8f341ac7e5f15af
Zoey <Azophyte@gmail.com>
2026-07-24 15:56:28 +0100

    Default nausea config is now 2

--- src/main/java/net/zoey/cozyliving/Config.java
+++ src/main/java/net/zoey/cozyliving/Config.java
@@ -6,7 +6,7 @@ public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
     private static final ModConfigSpec.DoubleValue DEBILITATING_NAUSEA_FACTOR = BUILDER
         .comment("How debilitating to make Third Eye Open")
-        .defineInRange("debilitatingNauseaFactor", 3., 1., Double.POSITIVE_INFINITY);
+        .defineInRange("debilitatingNauseaFactor", 2, 1., Double.POSITIVE_INFINITY);
     static final ModConfigSpec SPEC = BUILDER.build();
 
     public static double debilitatingNauseaFactor() {
