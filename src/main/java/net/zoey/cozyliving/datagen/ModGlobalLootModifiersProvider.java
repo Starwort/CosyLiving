0bb6764407041015e2dda5488a46d86dfa7c8bf4
Zoey <Azophyte@gmail.com>
2026-07-25 01:10:28 +0100

    Coconuts can be found in sunken ships

--- src/main/java/net/zoey/cozyliving/datagen/ModGlobalLootModifiersProvider.java
+++ src/main/java/net/zoey/cozyliving/datagen/ModGlobalLootModifiersProvider.java
@@ -22,6 +22,8 @@ public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
     @Override
     protected void start() {
 
+        //MODIFIED BLOCK DROPS
+
         add("cinnamon_from_jungle_leaves", new AddItemModifier(new LootItemCondition[] {
                 LootItemBlockStatePropertyCondition.hasBlockStateProperties(Blocks.JUNGLE_LEAVES).build(),
                 LootItemRandomChanceCondition.randomChance(0.186f).build()} //Drops from roughly one in 29 blocks (~twice per tree)
@@ -33,9 +35,6 @@ public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
                 LootItemRandomChanceCondition.randomChance(Mth.sqrt(0.008f)).build()} //Should drop one raspberry for roughly every sixteen seeds
                 , ModBlocks.RASPBERRY_BUSH.asItem()));
 
-        add("gilded_cinnamon_from_jungle_temple_chests", new AddItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("chests/jungle_temple")).build() }, ModItems.GILDED_CINNAMON_STICK.item()));
-
         add("red_sugar_from_redstone_ore", new AddItemModifier(new LootItemCondition[] {
                 LootItemBlockStatePropertyCondition.hasBlockStateProperties(Blocks.REDSTONE_ORE).build(),
                 LootItemRandomChanceCondition.randomChance(0.182f).build()} //Roughly one in thirty ores
@@ -45,5 +44,21 @@ public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
                 LootItemBlockStatePropertyCondition.hasBlockStateProperties(Blocks.DEEPSLATE_REDSTONE_ORE).build(),
                 LootItemRandomChanceCondition.randomChance(0.225f).build()} //Roughly one in twenty ores
                 , ModItems.Food.RED_SUGAR.item()));
+
+        //MODIFIED FISHING (SCRAPPED - THIS LITERALLY GUARANTEED A COCONUT WITH EVERY FISH)
+
+        /*add("coconut_from_fishing", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("gameplay/fishing/fish")).build() },
+                ModBlocks.COCONUT.asItem()));*/
+
+        //MODIFIED CHEST LOOT
+
+        //TODO: figure this shit out icba rn
+        add("coconut_from_sunken_ship", new AddItemModifier(new LootItemCondition[]{
+                new LootTableIdCondition.Builder(ResourceLocation.withDefaultNamespace("chests/shipwreck_supply")).build(),
+                LootItemRandomChanceCondition.randomChance(0.33f).build(),
+        }
+                , ModBlocks.COCONUT.item()));
+
     }
 }
